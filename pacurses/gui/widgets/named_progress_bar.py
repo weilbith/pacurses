098734fbb415c4f5e8e838711c58e89f0cb87d2b
Thore Weilbier <thore@weilbier.net>
2019-02-27 22:14:53 +0100

    Support volume bar higher than 100%.

--- pacurses/gui/widgets/named_progress_bar.py
+++ pacurses/gui/widgets/named_progress_bar.py
@@ -8,24 +8,13 @@ TEXT_PREFIX_WIDTH = 7  # In maximum 3 digits plus percentage character and minus
 
 class NamedProgressBar(ProgressBar):
     def __init__(
-        self,
-        normal,
-        complete,
-        current=0,
-        done=100,
-        satt=None,
-        name="",
-        width=20,
+        self, normal, complete, current=0, done=None, satt=None, name="", width=20
     ):
         self.name = name
         self.max_text_length = width - TEXT_PREFIX_WIDTH
+        done = (int(current / 100) + 1) * 100 if not done else done
         super(NamedProgressBar, self).__init__(normal, complete, current, done, satt)
 
     def get_text(self):
         name = abbreviate_text(self.name, self.max_text_length)
-        name = " - {0}".format(name) if self.name else ""
-
-        percentage = min(100, max(0, int(self.current * 100 / self.done)))
-        percentage = "{0}%".format(str(percentage))
-
-        return percentage + name
+        return f"{self.current}% - {name}"
